0b204aba9260397c9eecbd4a174562266eb22c94
Roland Levy <rolandjlevy@gmail.com>
2020-11-06 18:49:16 +0000

    Completed scoring

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { $, $$, delay, randomNum, getRandomNumbers } from './src/utils.js';
-import { scoring } from './src/scoring.js';
+import { scoreValues, checkScore } from './src/scoring.js';
 
-const nums = getRandomNumbers(3, 10);
-console.log({nums})
-
-let credits = 11;
+let credits = 2;
+let score = 0;
+let winObject = {}
+const containerRect = $('.container').getBoundingClientRect();
 
 window.startReels = function() {
   credits--;
@@ -18,6 +18,7 @@ window.startReels = function() {
 
 window.stopReels = async function() {
   $('.btn.stop').classList.remove('active');
+  winObject = {};
   const nums = getRandomNumbers(3, 10);
   await delay(1500);
   stopAnimation('r1', nums[0]);
@@ -35,24 +36,40 @@ window.toggleReel = async function(reel, btn) {
 }
 
 function stopAnimation(reelName, pos, state) {
-  const topOffset = 379;
+  const topOffset = 312;
   const reel = $(`.reel.${reelName}`);
   const timer = setInterval(() => {
     const rect = reel.firstElementChild.getBoundingClientRect();
     const top = rect.top + topOffset;
-    if (top >= (pos * 100) - 100 && top <= (pos * 100) - 90) {
-      if (reelName === 'r3') {
-        $('.btn.start').classList.add('active');
+    if (top >= (pos * 100) - 60 && top <= (pos * 100) - 50) {
+      getSelectedEmoji(reelName);
+      if (Object.keys(winObject).length == 3) {
+        const winStr = Object.values(winObject).join('');
+        score += checkScore(winStr);
+        $('.score-display').textContent = String(score);
+        if (credits > 0) {
+          $('.btn.start').classList.add('active');
+        } else {
+          console.log('Restart game...');
+        }
       }
-      console.log({pos})
       reel.classList.add('stop');
       clearInterval(timer);
     }
   }, 1);
 }
 
+function getSelectedEmoji(reelName) {
+  $$(`.${reelName} > li`).forEach(item => {
+    const fruitTop = item.getBoundingClientRect().top + item.clientHeight / 2;
+    if (fruitTop > containerRect.top && fruitTop < containerRect.bottom) {
+      winObject[reelName] = item.classList.value;
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', async (e) => {
   $('main.wrapper').classList.remove('init');
-  await delay(3000);
+  await delay(2000);
   startReels();
 });
