0278f565a13ccb9356970537e29baf195dc51470
Roland Levy <rolandjlevy@gmail.com>
2020-11-08 14:02:10 +0000

    Trying to get Babel to work

---
+++ dist/Reels.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Reels = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Reels = /*#__PURE__*/function () {
+  function Reels() {
+    _classCallCheck(this, Reels);
+  }
+
+  _createClass(Reels, [{
+    key: "init",
+    value: function init() {}
+  }]);
+
+  return Reels;
+}();
+
+exports.Reels = Reels;
