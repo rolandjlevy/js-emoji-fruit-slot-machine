9caefa35aa39c1d434e4e7238b7327b34603945f
Roland Levy <rolandjlevy@gmail.com>
2020-11-23 22:06:42 +0000

    Added points to scoring for just one fruit

--- src/Score.js
+++ src/Score.js
@@ -15,23 +15,28 @@ export class Score extends Utils {
     this.values = {
       'ggg': 300,
       'gg': 100,
+      'g': 25,
       'ooo': 240,
       'oo': 80,
+      'o': 20,
       'ccc': 180,
       'cc': 60,
+      'c': 15,
       'lll': 120,
-      'll': 40
+      'll': 40,
+      'l': 10
     }
   }
   getValue = function() {
     const str3 = Object.values(this.win).join('');
-    const regex = /^(.)\1+$/;
+    const regex = /^(.)\1+$/; // first char is repeated one or more times and occupies entire test string
     const win3 = regex.test(str3) && str3.length == 3;
     if (win3) return this.values[str3];
     const str2 = str3.slice(0, 2);
     const win2 = regex.test(str2) && str2.length == 2;
     if (win2) return this.values[str2];
-    return 0;
+    const str1 = str3.slice(0, 1);
+    return this.values[str1];
   }
   getLeaderBoard({url, maxIndex}) {
     this.$('.leader-board-points').innerHTML = '<li>Loading...</li>';
